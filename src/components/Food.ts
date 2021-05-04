bd408d0f9ef695c0de6de7e85492372e24f0f5ce
JCS <jcsweiyi@outlook.com>
2021-05-04 13:11:52 +0800

    finished

--- src/components/Food.ts
+++ src/components/Food.ts
@@ -18,8 +18,8 @@ class Food {
     //生成一个随机位置
     //食物位置[0,290]
     //蛇移动一次就是一格，一格大小就是10，所以要求食物的坐标必须是整10
-    let top = Math.round((Math.random() * 29) * 10) + 'px';
-    let left = Math.round((Math.random() * 29) * 10) + 'px';
+    let top = (Math.round(Math.random() * 29) * 10) + 'px';
+    let left = (Math.round(Math.random() * 29) * 10) + 'px';
     this.element.style.left = left;
     this.element.style.top = top;
   }
--- src/components/Snake.ts
+++ src/components/Snake.ts
@@ -25,6 +25,19 @@ class Snake {
       //蛇撞墙，抛出异常，游戏结束
       throw new Error("蛇撞墙了");
     }
+    //修改X值，实在修改水平坐标，蛇在左右移动，
+    //蛇在向左移动的时候，不能调头，反之亦然
+    if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+      //如果发生了掉头，让蛇向反方向继续移动
+      if (value > this.X) {
+        //如果新值 value 大于 X,说明蛇向左走，此时发生调头，应该使蛇继续向左走
+        value = this.X - 10;
+      } else {
+        value = this.X + 10;
+      }
+    }
+    //移动身体
+    this.moveBody();
     this.head.style.left = value + 'px';
   }
   set Y(value: number) {
@@ -33,12 +46,28 @@ class Snake {
       //蛇撞墙，游戏结束
       throw new Error("蛇撞墙了");
     }
+    //移动身体
+    this.moveBody();
     this.head.style.top = value + 'px';
   }
 
   //蛇增加身体
   addBody() {
     this.element.insertAdjacentElement('beforeend', document.createElement('div'));
+  }
+
+  //添加一个蛇身体移动的方法
+  moveBody() {
+    //将后边身体设置为前边身体的问题
+    //遍历获取所有身体
+    for (let i = this.bodies.length - 1; i > 0; i--) {
+      //获取前边身体的位置
+      let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+      let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+      //将获取的值设置到当前的身体
+      (this.bodies[i] as HTMLElement).style.left = X + 'px';
+      (this.bodies[i] as HTMLElement).style.top = Y + 'px';
+    }
   }
 }
 
