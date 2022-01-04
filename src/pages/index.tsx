ce2416dc93cb5881c16b21fd8a7c08d59d24fc64
Chuangshi Jiang <18251984729@163.com>
2022-01-04 19:28:18 +0800

    react source code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,5 +4,6 @@ import { lazy } from 'react';
 const Home = lazy(() => import('./home'));
 const Home2 = lazy(() => import('./home2'));
 const Jiemi = lazy(() => import('./jiemicss'));
+const ReactSourceCode = lazy(() => import('./react-source-code'));
 
-export { Home, Home2, Jiemi };
+export { Home, Home2, Jiemi, ReactSourceCode };
---
+++ src/pages/react-source-code/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReactSourceCode: React.FC = () => {
+  return <>source code</>;
+};
+
+export default ReactSourceCode;
