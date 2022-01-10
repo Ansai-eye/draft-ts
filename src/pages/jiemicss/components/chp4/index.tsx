7f16cfdf0bd816415a54e1399f7e36e5aeb8e42e
Chuangshi Jiang <18251984729@163.com>
2022-01-10 15:20:13 +0800

    35

--- src/pages/jiemicss/components/chp4/index.tsx
+++ src/pages/jiemicss/components/chp4/index.tsx
@@ -1,84 +1,136 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, Dispatch, SetStateAction, ReactNode, useEffect, InputHTMLAttributes } from 'react';
+import testimg from '@/assets/img/test.jpg';
+import testimg2 from '@/assets/img/test2.jpg';
 import './index.less';
 
+type MainProps = {
+  setVisi: Dispatch<SetStateAction<boolean>>;
+  visi: boolean;
+  children?: ReactNode;
+};
+
 const Chapter4: React.FC = () => {
+  const [visi, setVisi] = useState(false);
+  const props: MainProps = {
+    visi,
+    setVisi
+  };
   return (
     <>
-      <section className="bevel"></section>
-      <hr />
-      <section className="hyphen">
-        The only way to get rid of a temptation is to yield to it.
-      </section>
-      <hr />
-      <section className="break-line">
-        <dl>
-          <dt>Name:</dt>
-          <dd>Lea Verou</dd>
-
-          <dt>Email:</dt>
-          <dd>18251984729@163.com</dd>
-          <dd>142404@qq.com</dd>
-
-          <dt>Location:</dt>
-          <dd>Earth China</dd>
-        </dl>
-      </section>
-      <hr />
-      <section className="stripe-under-text">
-        <code>
-          while (true) {'{'} <br />
-          &nbsp;var d = new Date();
-          <br />
-          &nbsp;if(d.getDate() == 1 && d.getMonth() == 3) {'{'} <br />
-          &nbsp;&nbsp;alert(Hello); <br />
-          &nbsp;{'}'} <br />
-          {'}'}
-        </code>
-      </section>
-      <hr />
-      <section className="text-decoration">
-        The only way to{' '}
-        <a href="https://www.baidu.com">get rid of a temptation</a> is to{' '}
-        <a href="https://www.baidu.com">yield</a> to it.
-      </section>
-      <hr />
-      <section className="word-effect">
-        &ldquo;The only way to get rid of a temptation is to yield to it.&rdquo;
-      </section>
-      <section className="word-effect-2">
-        <h1>
-          <svg width="2em" height="1.2em">
-            <use xlinkHref="#css" />
-            <text id="css" y="1em">
-              CSS
-            </text>
-          </svg>
-        </h1>
-      </section>
-      <section className="word-effect-light">Glow</section>
-      <section className="word-effect-circle">
-        <svg viewBox="0 0 100 100">
-          <path d="M 0,50 a 50,50 0 1,1 0,1 z" id="circle" />
-          <text>
-            <textPath xlinkHref="#circle">
-              circular reasoning works because hello moto
-            </textPath>
-          </text>
-        </svg>
-      </section>
-      <hr />
-      <section className="cursor disabled"></section>
-      <hr />
-      <section className="checkbox-customize">
-        <input type="checkbox" name="checkbox" id="awsome" />
-        <label htmlFor="awsome">Awsome!</label>
-      </section>
+      <Main {...props} />
       {
-        false && ReactDOM.createPortal(<div className="modal"></div>, document.body)
+        visi &&
+        <div className="modal" onClick={() => setVisi(false)}>
+          O hai, I&apos;m a dialog. Click on me to dismiss.
+        </div>
       }
     </>
   );
+};
+
+const Main: React.FC<MainProps> = (props: MainProps) => {
+  const { visi, setVisi } = props;
+  useEffect(()=>{
+    // 创建附加div，并用它包住第一个图片元素
+    const div = document.createElement('div');
+    const img = document.querySelector('img') as Node;
+    const slider = document.querySelector('.iteractive-img');
+    slider?.insertBefore(div,img);
+    div.appendChild(img);
+    // 创建滑块
+    const range = document.createElement('input');
+    range.type = 'range';
+    range.oninput = function() {
+      div.style.width = (this as InputHTMLAttributes<Range>).value + '%';
+    };
+    slider?.appendChild(range);
+  },[]);
+  return <main className={`${visi ? 'de-emphasized' : ''}`}>
+    <section className="bevel"></section>
+    <hr />
+    <section className="hyphen">
+      The only way to get rid of a temptation is to yield to it.
+    </section>
+    <hr />
+    <section className="break-line">
+      <dl>
+        <dt>Name:</dt>
+        <dd>Lea Verou</dd>
+
+        <dt>Email:</dt>
+        <dd>18251984729@163.com</dd>
+        <dd>142404@qq.com</dd>
+
+        <dt>Location:</dt>
+        <dd>Earth China</dd>
+      </dl>
+    </section>
+    <hr />
+    <section className="stripe-under-text">
+      <code>
+        while (true) {'{'} <br />
+        &nbsp;var d = new Date();
+        <br />
+        &nbsp;if(d.getDate() == 1 && d.getMonth() == 3) {'{'} <br />
+        &nbsp;&nbsp;alert(Hello); <br />
+        &nbsp;{'}'} <br />
+        {'}'}
+      </code>
+    </section>
+    <hr />
+    <section className="text-decoration">
+      The only way to{' '}
+      <a href="https://www.baidu.com">get rid of a temptation</a> is to{' '}
+      <a href="https://www.baidu.com">yield</a> to it.
+    </section>
+    <hr />
+    <section className="word-effect">
+      &ldquo;The only way to get rid of a temptation is to yield to it.&rdquo;
+    </section>
+    <section className="word-effect-2">
+      <h1>
+        <svg width="2em" height="1.2em">
+          <use xlinkHref="#css" />
+          <text id="css" y="1em">
+            CSS
+          </text>
+        </svg>
+      </h1>
+    </section>
+    <section className="word-effect-light">Glow</section>
+    <section className="word-effect-circle">
+      <svg viewBox="0 0 100 100">
+        <path d="M 0,50 a 50,50 0 1,1 0,1 z" id="circle" />
+        <text>
+          <textPath xlinkHref="#circle">
+            circular reasoning works because hello moto
+          </textPath>
+        </text>
+      </svg>
+    </section>
+    <hr />
+    <section className="cursor disabled"></section>
+    <hr />
+    <section className="checkbox-customize">
+      <input checked={visi} type="checkbox" name="checkbox" id="awsome" onChange={e => setVisi(e.target.checked)} />
+      <label htmlFor="awsome">Awsome!</label>
+    </section>
+    <section className="scroll-reminder">
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+      <h2>Scroll reminder</h2>
+    </section>
+    <section className="iteractive-img">
+      <img src={testimg} alt="Before" />
+      <img src={testimg2} alt="After" />
+    </section>
+  </main>;
 };
 
 export default Chapter4;
--- src/pages/react-source-code/index.tsx
+++ src/pages/react-source-code/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ReactSourceCode: React.FC = () => {
-  return <>source code</>;
+  return <Father>
+    <Child />
+  </Father>;
+};
+
+type FatherProps = {
+  children?: React.ReactNode;
+};
+
+const Child: React.FC = () => {
+  console.log('Child');
+  return <div>Children</div>;
+};
+
+const Father: React.FC = (props: FatherProps) => {
+  const [num, setNum] = useState<number>(0);
+  return <div onClick={() => setNum(num + 1)}>
+    {num}
+    {props.children}
+  </div>;
 };
 
 export default ReactSourceCode;
