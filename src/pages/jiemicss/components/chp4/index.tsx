a277673de7701482d3455ad2be8f875f5ef9582f
Chuangshi Jiang <18251984729@163.com>
2022-01-11 15:57:09 +0800

    40

--- src/pages/jiemicss/components/chp4/index.tsx
+++ src/pages/jiemicss/components/chp4/index.tsx
@@ -30,12 +30,12 @@ const Chapter4: React.FC = () => {
 
 const Main: React.FC<MainProps> = (props: MainProps) => {
   const { visi, setVisi } = props;
-  useEffect(()=>{
+  useEffect(() => {
     // 创建附加div，并用它包住第一个图片元素
     const div = document.createElement('div');
     const img = document.querySelector('img') as Node;
     const slider = document.querySelector('.iteractive-img');
-    slider?.insertBefore(div,img);
+    slider?.insertBefore(div, img);
     div.appendChild(img);
     // 创建滑块
     const range = document.createElement('input');
@@ -44,7 +44,7 @@ const Main: React.FC<MainProps> = (props: MainProps) => {
       div.style.width = (this as InputHTMLAttributes<Range>).value + '%';
     };
     slider?.appendChild(range);
-  },[]);
+  }, []);
   return <main className={`${visi ? 'de-emphasized' : ''}`}>
     <section className="bevel"></section>
     <hr />
@@ -129,6 +129,27 @@ const Main: React.FC<MainProps> = (props: MainProps) => {
     <section className="iteractive-img">
       <img src={testimg} alt="Before" />
       <img src={testimg2} alt="After" />
+    </section>
+    <section className="self-adaption-content">
+      <p>当开发者走投无路时，就只能对figure应用一个固定的width或max-width了，然后对 应用 可是这个方法无法充分利用有效空间；对于过小的图片来说，布局效果也很突兀。此外，响应式也无从谈起。</p>
+      <figure>
+        <img src={testimg} width={300} alt="test" />
+        <figcaption>
+          目前这个渲染结果距离我们的期望还有不小的差距：文本行比图片要宽多了。如何让figure的宽度由它内部的图片来决定，而不是由它的父元素来决定呢
+        </figcaption>
+      </figure>
+      <p>当开发者走投无路时，就只能对figure应用一个固定的width或max-width了，然后对 应用 可是这个方法无法充分利用有效空间；对于过小的图片来说，布局效果也很突兀。此外，响应式也无从谈起。</p>
+    </section>
+    <footer>
+      <div className="wrapper">
+        这里是页脚的内容
+      </div>
+    </footer>
+    <section className="vertical-align">
+      <main>
+        <h1>Am I centerd yet?</h1>
+        <p>Center me, please!</p>
+      </main>
     </section>
   </main>;
 };
