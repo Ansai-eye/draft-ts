de3674836e732e3e5473d9abe9b34ae2d8546807
Chuangshi Jiang <18251984729@163.com>
2022-01-18 22:25:33 +0800

    eslint optimize and useRef test

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import React,{ useMemo, useState } from 'react';
+
+type DimentionOpt = {
+  propId: string;
+  propName: string;
+};
+
+let index = 0;
 
 export default function Home(): JSX.Element {
-  return <div>home 页面</div>;
+  const [chosenDimentionOpts,setChosenDimentionOpts] = useState<DimentionOpt[]>([]);
+  const opts = useMemo(() => {
+    const optArr = chosenDimentionOpts.map((el:DimentionOpt) => el.propId);
+    return optArr;
+  },[chosenDimentionOpts]);
+
+  const handleAddDimension = ():void => {
+    const item: DimentionOpt = {
+      propId: String(index),
+      propName: `hello${index}`
+    };
+    setChosenDimentionOpts([...chosenDimentionOpts,item]);
+    index ++;
+  };
+  return <div>
+    <ol>
+      {
+        opts.map((item:string) => {
+          return <li key={item}>{item}</li>;
+        })
+      }
+    </ol>
+    <button onClick={handleAddDimension}>Click to add dimension</button>
+  </div>;
 }
