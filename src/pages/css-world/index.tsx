005949626e212f9bc4b863ad0f19ad6ab11e19c2
Chuangshi Jiang <18251984729@163.com>
2022-01-20 11:38:55 +0800

    4

--- src/pages/css-world/index.tsx
+++ src/pages/css-world/index.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import './index.less';
 
 const CSSWorld: React.FC = () => {
-  return <>css world</>;
+  return <>
+    <a>css world</a>
+    <div className="clear">
+      <div className="float-left">
+        this is float element
+      </div>
+    </div>
+    <ol>
+      <li>li</li>
+    </ol>
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>table</td>
+        </tr>
+      </tbody>
+    </table>
+    <footer>footer</footer>
+    <div className="box">
+      <p className="content">hello</p>
+    </div>
+    <div className="ao"></div>
+    <div className="tu"></div>
+    <div className="height-auto">
+      <div className="child"></div>
+    </div>
+    <hr />
+    <div className="ghost-node">
+      <span></span>
+    </div>
+  </>;
 };
 
 export default CSSWorld;
