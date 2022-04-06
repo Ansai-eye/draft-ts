e716d22a57d0280c19b273475b9402c0cea91f01
Chuangshi Jiang <18251984729@163.com>
2022-04-06 13:14:04 +0800

    prepaire for sharing 2

--- src/pages/css-world/index.tsx
+++ src/pages/css-world/index.tsx
@@ -4,41 +4,58 @@ import './index.less';
 const CSSWorld: React.FC = () => {
   const str = '中国x Sphinx Aghfy';
   const strArr = str.split('');
-  const len = 3000;
   return <>
-    {/* <div className="container">
-      <span>x</span>
+    <div className="container">
       <img src="https://demo.cssworld.cn/images/common/m/1.jpg" />
     </div>
-    <div className="font-compaire en-write-line-1">
+
+    {/* <div className="strut"><span></span></div> */}
+
+    {/* 行高 */}
+    {/* <div className="demo-line-height">xxx</div> */}
+
+    {/* 行距，半行距 */}
+    {/* <div className="line-distance">
+      <span>sphinx</span>
+    </div> */}
+    {/* <div className="line-distance">
+      <span>sphinx</span>
+    </div> */}
+
+    {/* <div className="font-compaire en-write-line-1">
       <div className="median"></div>
       {
         strArr.map((item, index) => {
           return <span key={index} className="x-font x-font-1">{item}</span>;
         })
       }
-
     </div>
+
     <div className="font-compaire en-write-line-2">
       <div className="median"></div>
-      <span className="x-font x-font-2">中国x Sphinx Aghfy</span>
-    </div>
-    <div className="font-compaire en-write-line-3">
-      <div className="median"></div>
-      <span className="x-font x-font-3">中国x Sphinx Aghfy</span>
-    </div>
-    <span className="x-font x-font-alone x-font-3">中国x Sphinx Aghfy</span>
-    <div className="demo-line-height"></div>
-    <div className="line-distance">
-      <span>sphinx</span>
-    </div> */}
-    <ul>
       {
-        Array(len).fill(0).map((item,index) => {
-          return <li key={index}>{index}</li>;
+        strArr.map((item, index) => {
+          return <span key={index} className="x-font x-font-2">{item}</span>;
         })
       }
-    </ul>
+    </div> */}
+
+    {/* <div className="font-compaire en-write-line-3">
+      <div className="median"></div>
+      <span className="x-font x-font-3">中国x Sphinx Aghfy</span>
+    </div> */}
+
+    {/* <span className="x-font x-font-alone x-font-3">中国x Sphinx Aghfy</span> */}
+
+    {/* 行高对块级元素的影响 */}
+    {/* <div className="demo-block-line-height">
+      x<img height="96" src="https://demo.cssworld.cn/images/common/m/1.jpg" />
+    </div> */}
+
+    {/* vertical-align示例*/}
+    {/* <div className="demo-vertical-align">
+      <span className="x">x</span>&nbsp;<span className="text">文本x</span>
+    </div> */}
   </>;
 };
 
