488ab3ca2cf51cd5cfba5223825e6c9f97bdf665
Chuangshi Jiang <18251984729@163.com>
2022-05-24 11:21:44 +0800

    css new world 3.8.4

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+import './index.less';
+
 export default function Home(): JSX.Element {
-  return <div>home 页面</div>;
+  return <>
+    <span>这是第一句话</span>&nbsp;&nbsp;<span className="decoration">这是第二句话</span>
+    <div>这是第三句话</div>
+    <div>这是第四句话</div>
+    <section>
+      <p>父元素设置text-decoration: dashed underline;</p>
+      <p>子元素设置text-decoration: wavy overline;<wavy /></p>
+      
+    </section>
+  </>;
 }
