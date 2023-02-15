e9a967cf6d1cf07e4a08bbd00e19d16321d635e2
ansai <jcsweiyi@outlook.com>
2023-02-15 21:39:15 +0800

    modify tsconfig.json

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-
-import { usePow, useCreation } from '@/hooks';
-import './index.less';
-
-/* Make all properties in T optional */
-type Partial<T> = {
-  [P in keyof T]?: T[P];
-};
-
-/* Make all properties in T required */
-type Required<T> = {
-  [P in keyof T]-?: T[P];
-};
-
-type ReadOnly<T> = {
-  readonly [P in keyof T]: T[P];
-}
-
-interface IPerson {
-  name?: string;
-  age?: number;
-  height?: number;
-}
-
-const person: Partial<IPerson> = {
-  name: 'zhangsan'
-};
-
-const person2: Required<IPerson> = {
-  name: 'lisi',
-  age: 20,
-  height: 176,
-};
-
-console.log('person======', person);
-
-export default function Home(): JSX.Element {
-  const [flag, setFlag] = useState<boolean>(false);
-  const data = usePow([1, 2, 3]);
-
-  const getNewData = () => {
-    return Math.random();
-  };
-
-  const nowData = useCreation(() => getNewData(), []);
-
-  return <>
-    <span>这是第一句话</span>&nbsp;&nbsp;<span className="decoration">这是第二句话</span>
-    <div>这是第三句话</div>
-    <div>这是第四句话</div>
-    <section>
-      <p data-clstag="hello">父元素设置text-decoration: dashed underline;</p>
-      <p>子元素设置text-decoration: wavy overline;</p>
-    </section>
-    <section>
-      <div>数字：{JSON.stringify(data)}</div>
-      <button onClick={() => { setFlag(v => !v); }}>切换</button>
-      <div>切换状态：{JSON.stringify(flag)}</div>
-    </section>
-    <section>
-      <div>正常函数：{getNewData()}</div>
-      <div>useCreation包裹后的： {nowData}</div>
-      <button onClick={() => { setFlag(v => !v); }}>渲染</button>
-    </section>
-  </>;
-=======
 import React,{ useMemo, useState } from 'react';
 
 type DimentionOpt = {
@@ -100,5 +32,4 @@ export default function Home(): JSX.Element {
     </ol>
     <button onClick={handleAddDimension}>Click to add dimension</button>
   </div>;
->>>>>>> 1f048d6a77acc2575efa005357a02ae4ea11cd29
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,6 @@
 import * as webpack from 'webpack';
 import * as path from 'path';
-import HtmlWebpackPlugin = require('html-webpack-plugin');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const RootPath = path.join(process.cwd());
 const AppPath = path.join(RootPath, '/src');
@@ -79,7 +79,7 @@ const config: webpack.Configuration = {
         use: {
           loader: 'file-loader',
           options: {
-            name: '[folder]/[name].[hash:8].[ext]',
+            name: '[path]/[name].[hash:8].[ext]',
             outputPath: './static/assets',
           },
         },
